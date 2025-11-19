25f6240e003251a0fce063eebcee91c274cd4ace
lukas729 <lukas177585@gmail.com>
2025-11-19 15:01:55 -0300

    fiz3

---
+++ fiz3.js
@@ -0,0 +1,10 @@
+let numeros = [10, 20, 30, 40,];
+
+let mutli = numeros.map((numero) =>{
+    return numero * 2;
+});
+console.log(numeros);
+
+console.log("A multiplicação dos numero acima é:");
+
+console.log(mutli);
