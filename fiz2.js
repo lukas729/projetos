c1f9fa23ded3ca362e4be4da91f536f22f0d768b
lukas729 <lukas177585@gmail.com>
2025-11-19 14:48:39 -0300

    fiz2

---
+++ fiz2.js
@@ -0,0 +1,15 @@
+let nomes = ["Vitoria", "Leticia" ,"Beatriz"]
+
+nomes.push("Lukas jnr,", "Kaun jnr");
+console.log(nomes)
+
+let removido = nomes.pop();
+console.log(nomes)
+
+console.log( "Minha lista de nomes:", "\n")
+
+nomes.forEach((nomes,index) => {
+ 
+    console.log(`${index + 1}. ${nomes} `);
+ 
+});
