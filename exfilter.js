5f78daf089dc754850cb37192d9ae78a51c5a04d
lukas729 <lukas177585@gmail.com>
2025-11-19 16:54:12 -0300

    filter

---
+++ exfilter.js
@@ -0,0 +1,9 @@
+let notas = [4.5, 8.0 ,6.5 , 9.0, 3.0]
+
+let aprovados = notas.filter((notas) =>{
+    return notas >= 7;
+});
+
+console.log(notas);
+
+console.log(aprovados);
--- fiz4.js
+++ fiz4.js
@@ -2,10 +2,10 @@ let numero = [5, 15, 8, 25, 3, 18];
  
 let contador = 0
  
-numero.forEach((numero, index) =>{
+numero.forEach(numero =>{
  
      console.log(` ${numero} `);
- 
+   
     if(numero > 10){
         contador++;
     }
